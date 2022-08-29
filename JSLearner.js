e3245d84603df878595b09a0e29ccd92916127d5
piran777 <paminul@uwo.ca>
2022-08-29 16:29:40 -0400

    first commit

---
+++ JSLearner.js
@@ -0,0 +1,4 @@
+const computerChoice = document.getElementById('computer-choice')
+const userChoice = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button') //we only have 3 butttons and have no plan to add more so we jsut use quereyselectorall
